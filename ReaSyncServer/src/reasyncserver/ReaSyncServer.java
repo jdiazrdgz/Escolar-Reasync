2b07045136cf222f2a5ba62c890c2946e9ab3b2b
jdiazrdgz <jdiazrdgz@gmail.com>
2017-05-21 12:45:00 -0500

    Se añade la estrucutra de menu y ventanas para la aplicacion server

---
+++ ReaSyncServer/src/reasyncserver/ReaSyncServer.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package reasyncserver;
+
+/**
+ *
+ * @author jdiaz
+ */
+public class ReaSyncServer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        reasyncserver.vistas.ReaSyncServer frame =new  reasyncserver.vistas.ReaSyncServer();
+        frame.setVisible(true);
+    }
+    
+}
